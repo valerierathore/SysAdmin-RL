a3d0d8c13e4b2ac8d37864a7917828d42cf714db
valerierathore <valerierathore@gmail.com>
2026-04-08 18:05:10 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, request, jsonify
-from pydantic import BaseModel
 
 app = Flask(__name__)
 
@@ -12,9 +11,9 @@ def home():
 def reset():
     return jsonify({
         "status": "success",
-        "observation": "System Issues: 1. Service Down, 2. Disk Full, 3. Port Blocked.",
+        "observation": "System Issues Detected: Task 1: Web, Task 2: Disk, Task 3: Port.",
         "is_fixed": False,
-        "reward": 0.15
+        "reward": 0.123
     })
 
 @app.route('/state', methods=['GET'])
@@ -22,32 +21,32 @@ def state():
     return jsonify({
         "status": "active",
         "is_fixed": False,
-        "reward": 0.15,
-        "observation": "Waiting for task."
+        "reward": 0.123,
+        "observation": "Waiting for task execution."
     })
 
 @app.route('/step', methods=['POST'])
 def step():
     data = request.json
     action = data.get("action", "").lower()
-    task_id = data.get("task_id", 1)
+    task_id = int(data.get("task_id", 1))
     
     if task_id == 1:
         if "restart" in action:
-            return jsonify({"status": "success", "is_fixed": True, "reward": 0.85})
-        return jsonify({"status": "success", "is_fixed": False, "reward": 0.15})
+            return jsonify({"status": "success", "is_fixed": True, "reward": 0.881})
+        return jsonify({"status": "success", "is_fixed": False, "reward": 0.111})
     
     elif task_id == 2:
-        if "clean" in action or "clear" in action:
-            return jsonify({"status": "success", "is_fixed": True, "reward": 0.82})
-        return jsonify({"status": "success", "is_fixed": False, "reward": 0.15})
+        if "clean" in action:
+            return jsonify({"status": "success", "is_fixed": True, "reward": 0.772})
+        return jsonify({"status": "success", "is_fixed": False, "reward": 0.112})
     
     elif task_id == 3:
-        if "unblock" in action or "port" in action:
-            return jsonify({"status": "success", "is_fixed": True, "reward": 0.78})
-        return jsonify({"status": "success", "is_fixed": False, "reward": 0.15})
+        if "unblock" in action:
+            return jsonify({"status": "success", "is_fixed": True, "reward": 0.663})
+        return jsonify({"status": "success", "is_fixed": False, "reward": 0.113})
 
-    return jsonify({"status": "error", "reward": 0.15})
+    return jsonify({"status": "error", "reward": 0.055})
 
 def main():
     app.run(host="0.0.0.0", port=7860)
