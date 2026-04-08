54032a74d2ca8f8f5957c55faab7d4efed469fb1
valerierathore <valerierathore@gmail.com>
2026-04-08 20:52:27 +0530

    Update graders.py

--- server/graders.py
+++ server/graders.py
@@ -1,8 +1,8 @@
-def easy_grader(*args, **kwargs):
+def easy_grader(output=None, **kwargs):
     return 0.85
 
-def medium_grader(*args, **kwargs):
+def medium_grader(output=None, **kwargs):
     return 0.75
 
-def hard_grader(*args, **kwargs):
+def hard_grader(output=None, **kwargs):
     return 0.65
