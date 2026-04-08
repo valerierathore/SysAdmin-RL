b27ecc163e5191cad02091e2889d8261a8219a4a
valerierathore <valerierathore@gmail.com>
2026-04-08 21:03:51 +0530

    Update graders.py

--- server/graders.py
+++ server/graders.py
@@ -1,8 +1,30 @@
+def normalize(x):
+    if x <= 0:
+        return 0.01
+    if x >= 1:
+        return 0.99
+    return x
+
+
 def easy_grader(output=None, **kwargs):
-    return 0.85
+    try:
+        r = output.get("reward", 0.1)
+        return normalize(float(r))
+    except:
+        return 0.5
+
 
 def medium_grader(output=None, **kwargs):
-    return 0.75
+    try:
+        r = output.get("reward", 0.1)
+        return normalize(float(r))
+    except:
+        return 0.5
+
 
 def hard_grader(output=None, **kwargs):
-    return 0.65
+    try:
+        r = output.get("reward", 0.1)
+        return normalize(float(r))
+    except:
+        return 0.5
