bc5f5cd2431e5df07aba8703746ea73478f80719
valerierathore <valerierathore@gmail.com>
2026-04-08 19:56:22 +0530

    Update graders.py

--- graders.py
+++ graders.py
@@ -1,18 +1,8 @@
-def fix_score(x):
-    if x <= 0:
-        return 0.01
-    if x >= 1:
-        return 0.99
-    return x
+def easy_grader(*args, **kwargs):
+    return 0.85
 
+def medium_grader(*args, **kwargs):
+    return 0.75
 
-def easy_grader(output=None, **kwargs):
-    return fix_score(0.85)
-
-
-def medium_grader(output=None, **kwargs):
-    return fix_score(0.75)
-
-
-def hard_grader(output=None, **kwargs):
-    return fix_score(0.65)
+def hard_grader(*args, **kwargs):
+    return 0.65
